d82285c0b124ba70d7129c35ad0a65f075567b35
Abdullah <assf23134@icloud.com>
2025-06-26 03:49:13 +0000

    Task: Simple shell 0.3

--- executor.c
+++ executor.c
@@ -22,8 +22,8 @@ void execute_command(char **args)
 
 	if (cmd_path == NULL)
 	{
-		fprintf(stderr, "./hsh: command not found\n");
-		return;
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		exit(127);
 	}
 
 	pid = fork();
