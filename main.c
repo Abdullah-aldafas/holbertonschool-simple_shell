bf58a6b550695c394ea0fa793ba8fae2af9c6c4a
Abdullah <assf23134@icloud.com>
2025-07-02 22:17:52 +0000

    Refactor main.c for Betty compliance and update shell.h accordingly

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ int main(void)
 	size_t len = 0;
 	char *args[64];
 	int status = 0;
+	char *hash = strchr(line, '#');
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("$ ");
+
 		if (getline(&line, &len, stdin) == -1)
 		{
 			if (isatty(STDIN_FILENO))
@@ -27,14 +29,24 @@ int main(void)
 			free(line);
 			exit(status);
 		}
+
 		line[strcspn(line, "\n")] = '\0';
-		tokenize(line, args);
-		if (args[0] == NULL)
+
+		if (hash && (hash == line || *(hash - 1) == ' '))
+			*hash = '\0';
+
+		if (line[0] == '\0' || line[0] == '#')
 			continue;
+
+		tokenize(line, args);
+		if (args[0] == NULL || args[0][0] == '#')
+			continue;
+
 		if (handle_exit_env(args, line, &status))
 			continue;
+
 		status = execute_command(args);
 	}
 	free(line);
-	return (0);
+	return (status);
 }
